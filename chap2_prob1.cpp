8856ee1c2b66a976a2137b4d4ae7c6fb562e7d3c
reubencapio <reubencapio@gmail.com>
2016-04-07 01:40:33 +0800

    Create chap2_prob1.cpp

---
+++ chap2_prob1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+class LinkedList
+{
+public:
+	Node* head;
+};
+
+void removeDuplicates(LinkedList a_list)
+{
+
+}
+
+
+int main()
+{
+	LinkedList a_list = new LinkedList();
+	removeDuplicates(a_list);
+}
