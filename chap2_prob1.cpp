5cf5f7c7e08ad5df2166439f7bfb9bfafa969caa
reubencapio <reubencapio@gmail.com>
2016-04-08 11:25:59 +0800

    Update chap2_prob1.cpp

--- chap2_prob1.cpp
+++ chap2_prob1.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <unordered_set>
 #include "LinkedList.h"
 
-void removeDuplicates(LinkedList *a_list)
-{
 
+void removeDuplicates(Node *head)
+{
+	std::unordered_set<int> unique;
+	Node *current = head;
+	unique.insert(current->data);
+
+	while(current->next != nullptr)
+	{
+		if(unique.find(current->next->data) == unique.end()){
+			unique.insert(current->next->data);
+			current = current->next;
+		} 
+		else
+		{
+			Node* temp = current->next;
+			current->next = current->next->next;
+			delete temp;
+		}
+	}
 }
 
 
@@ -17,5 +33,6 @@ int main()
 	a_list->insert(3);
 	a_list->insert(5);
 	a_list->display();
-	removeDuplicates(a_list);
+	removeDuplicates(a_list->head);
+	a_list->display();
 }
